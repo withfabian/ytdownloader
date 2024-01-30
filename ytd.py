f1869b72dc9558c2356fb96cdcb5c2a913fa3e60
Fabian Permadi <128126769+withfabian@users.noreply.github.com>
2024-01-30 18:24:09 +0700

    Update ytd.py

--- ytd.py
+++ ytd.py
@@ -1,4 +1,22 @@
 from pytube import YouTube
+from colorama import Fore, Back, Style
+import pyfiglet
+
+def show_banner():
+    banner = pyfiglet.figlet_format("YT Downloader")
+    rainbow_colors = [Fore.RED, Fore.YELLOW, Fore.GREEN, Fore.BLUE, Fore.MAGENTA, Fore.CYAN]
+    rainbow_banner = ''
+    for i, char in enumerate(banner):
+        rainbow_banner += rainbow_colors[i % len(rainbow_colors)] + char
+    print(rainbow_banner)
+
+def show_menu():
+    print("╔═════════════════════════════════════════╗")
+    print("║               \033[1;33m🌟  MENU  🌟\033[0m              ║")
+    print("╠═════════════════════════════════════════╣")
+    print("║  \033[1;36m1\033[0m. Unduh video dari YouTube            ║")
+    print("║  \033[1;36m2\033[0m. Keluar                              ║")
+    print("╚═════════════════════════════════════════╝")
 
 def download_video(url, output_path='.'):
     try:
@@ -15,5 +33,16 @@ def download_video(url, output_path='.'):
         print(f"Terjadi kesalahan: {str(e)}")
 
 if __name__ == "__main__":
-    video_url = input("Masukkan URL video YouTube: ")
-    download_video(video_url, "./")
+    show_banner()
+    while True:
+        show_menu()
+        choice = input("Pilih menu: ")
+
+        if choice == '1':
+            video_url = input("Masukkan URL video YouTube: ")
+            download_video(video_url, "./")
+        elif choice == '2':
+            print("Terima kasih telah menggunakan program ini.")
+            break
+        else:
+            print("Pilihan tidak valid. Silakan pilih opsi yang benar.")
