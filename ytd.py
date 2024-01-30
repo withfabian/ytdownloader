02fa9e2798e573cae56667b4de38416bc8ac5fb3
Fabian Permadi <128126769+withfabian@users.noreply.github.com>
2024-01-31 00:01:31 +0700

    Update ytd.py

--- ytd.py
+++ ytd.py
@@ -1,14 +1,20 @@
 from pytube import YouTube
-from colorama import Fore, Back, Style
+from colorama import Fore, Style
 import pyfiglet
+import os
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def show_banner():
-    banner = pyfiglet.figlet_format("YT Downloader")
+    clear_screen()
+    banner = pyfiglet.figlet_format("YT Downloader", font="slant")
     rainbow_colors = [Fore.RED, Fore.YELLOW, Fore.GREEN, Fore.BLUE, Fore.MAGENTA, Fore.CYAN]
     rainbow_banner = ''
     for i, char in enumerate(banner):
         rainbow_banner += rainbow_colors[i % len(rainbow_colors)] + char
     print(rainbow_banner)
+    print()
 
 def show_menu():
     print("╔═════════════════════════════════════════╗")
@@ -17,32 +23,32 @@ def show_menu():
     print("║  \033[1;36m1\033[0m. Unduh video dari YouTube            ║")
     print("║  \033[1;36m2\033[0m. Keluar                              ║")
     print("╚═════════════════════════════════════════╝")
+    print()
 
 def download_video(url, output_path='.'):
     try:
+        show_banner()
+        print("Memulai pengunduhan...\n")
         yt = YouTube(url)
-        
-        # Mendapatkan stream video dengan kualitas tertinggi yang mencakup baik video maupun audio
         video_stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
-
-        # Mendownload video ke output_path
         video_stream.download(output_path)
-
-        print("Unduhan selesai!")
+        print("\nPengunduhan selesai!")
     except Exception as e:
-        print(f"Terjadi kesalahan: {str(e)}")
+        print(f"\nTerjadi kesalahan: {str(e)}")
 
 if __name__ == "__main__":
-    show_banner()
     while True:
+        show_banner()
         show_menu()
         choice = input("Pilih menu: ")
 
         if choice == '1':
             video_url = input("Masukkan URL video YouTube: ")
-            download_video(video_url, "./")
+            download_video(video_url, "./youtube")
+            input("\nTekan Enter untuk kembali ke menu...")
         elif choice == '2':
-            print("Terima kasih telah menggunakan program ini.")
+            print("\nTerima kasih telah menggunakan program ini.")
             break
         else:
-            print("Pilihan tidak valid. Silakan pilih opsi yang benar.")
+            print("\nPilihan tidak valid. Silakan pilih opsi yang benar.")
+            input("\nTekan Enter untuk kembali ke menu...")
